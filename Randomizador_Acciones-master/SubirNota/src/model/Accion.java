aa42c1e3efd3bd116eaeee5c46ac84dbdbda74b5
Footchiken <38819933+Footchiken@users.noreply.github.com>
2018-09-14 21:02:31 -0300

    Add files via upload

---
+++ Randomizador_Acciones-master/SubirNota/src/model/Accion.java
@@ -0,0 +1,25 @@
+
+package model;
+
+public class Accion {
+    private int id;
+    private String accion;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAccion() {
+        return accion;
+    }
+
+    public void setAccion(String accion) {
+        this.accion = accion;
+    }
+
+
+}
